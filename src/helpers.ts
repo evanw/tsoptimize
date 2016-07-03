9a16f53d4208a6816b1005fab8b7180f6f32c362
Evan Wallace <evan.exe@gmail.com>
2016-07-02 23:59:53 -0700

    speed up tests by caching lib.d.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,20 +2,18 @@ import * as fs from 'fs';
 import * as ts from 'typescript';
 
 let SyntaxKind = ts.SyntaxKind;
-
-let lib_d_ts: string = null;
+let librarySource: string = null;
+let libraryFile: ts.SourceFile = null;
 
 export function createProgram(files: {[path: string]: string}, options: ts.CompilerOptions): ts.Program {
-  let fileNames = Object.keys(files);
-  let clone: {[path: string]: string} = Object.create(null);
-  fileNames.forEach(fileName => clone[fileName] = files[fileName]);
-  clone['lib.d.ts'] = lib_d_ts || (lib_d_ts = fs.readFileSync(__dirname + '/../node_modules/typescript/lib/lib.d.ts', 'utf8'));
-  files = clone;
-
   let fileExists = (fileName: string) => fileName in files;
   let readFile = (fileName: string) => files[fileName];
   let host: ts.CompilerHost = {
     getSourceFile: (fileName, languageVersion) => {
+      if (fileName === 'lib.d.ts') {
+        let source = librarySource || (librarySource = fs.readFileSync(__dirname + '/../node_modules/typescript/lib/lib.d.ts', 'utf8'));
+        return libraryFile || (libraryFile = ts.createSourceFile(fileName, source, languageVersion));
+      }
       if (fileName in files) return ts.createSourceFile(fileName, files[fileName], languageVersion);
       return;
     },
@@ -36,5 +34,5 @@ export function createProgram(files: {[path: string]: string}, options: ts.Compi
     },
   };
 
-  return ts.createProgram(fileNames, options, host);
+  return ts.createProgram(Object.keys(files), options, host);
 }
